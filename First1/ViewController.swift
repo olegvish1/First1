cd8551475d20c130a63f80bfae654f19bd8c1b91
Oleg <olegvish81@gmail.com>
2021-04-17 12:26:08 +0300

    Initial Commit

---
+++ First1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First1
+//
+//  Created by Oleg Vishnivetskiy on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
