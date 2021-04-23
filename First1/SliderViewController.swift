0c778c8aa13615babd95938b829c4d18dd786524
Oleg <olegvish81@gmail.com>
2021-04-23 08:19:35 +0300

    add slider view controller

---
+++ First1/SliderViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SliderViewController.swift
+//  First1
+//
+//  Created by Oleg Vishnivetskiy on 23.04.2021.
+//
+
+import UIKit
+
+class SliderViewController: UIViewController {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var coverView: UIView!
+    @IBOutlet var shadowSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Slider"
+
+        setupContentView()
+
+        coverView.clipsToBounds = true
+        coverView.layer.cornerRadius = 15
+
+        coverView.alpha = CGFloat(slider.value)
+        contentView.clipsToBounds = shadowSwitch.isOn
+    }
+
+    @IBAction func sliderAction(_ sender: UISlider) {
+        coverView.alpha = CGFloat(sender.value)
+    }
+
+    @IBAction func changeColors() {
+
+        slider.thumbTintColor = UIColor.init(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        let leftColor: UIColor = .green
+        let rightColor = UIColor.myRedColor
+
+        slider.minimumTrackTintColor = leftColor
+        slider.maximumTrackTintColor = rightColor
+
+        contentView.backgroundColor = leftColor
+        coverView.backgroundColor = rightColor
+    }
+
+    @IBAction func switchAction(_ sender: UISwitch) {
+        contentView.clipsToBounds = sender.isOn
+    }
+}
+
+extension SliderViewController {
+
+    func setupContentView() {
+
+        contentView.layer.cornerRadius = 15
+
+        addShadow()
+    }
+    func addShadow() {
+        contentView.layer.shadowColor = UIColor.black.cgColor
+        contentView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        contentView.layer.shadowRadius = 15
+        contentView.layer.shadowOpacity = 0.5
+    }
+}
+
+extension UIColor {
+
+    static var myRedColor: UIColor {
+        return UIColor.init(red: 1, green: 0.336, blue: 0.071, alpha: 1)
+    }
+}
