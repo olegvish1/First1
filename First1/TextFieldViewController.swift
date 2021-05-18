b3e8dc7e6489cfe9711c4bf667091787059a79bb
Oleg <olegvish81@gmail.com>
2021-05-18 19:47:45 +0300

    add UITextFieldController

---
+++ First1/TextFieldViewController.swift
@@ -0,0 +1,78 @@
+//
+//  TextFieldViewController.swift
+//  First1
+//
+//  Created by Oleg Vishnivetskiy on 20.04.2021.
+//
+
+import UIKit
+
+class TextFieldViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Purple"
+        loginTextField.delegate = self
+        passTextField.delegate = self
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        loginTextField.becomeFirstResponder()
+
+    }
+
+    @IBAction func textChanged(_ sender: UITextField) {
+        print(sender.text ?? "")
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        if textField == loginTextField {
+            passTextField.becomeFirstResponder()
+        } else {
+            passTextField.resignFirstResponder()
+        }
+
+        return true
+    }
+
+    @IBAction func passEditing(_ sender: UITextField) {
+        print(sender.text ?? "")
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(textField.text ?? "")
+        print(range)
+        print(string)
+
+        if string == "r" {
+            return false
+        }
+
+        guard
+            let text = textField.text,
+            let textRange = Range(range, in: text)
+        else { return false }
+
+        let updatedText = text.replacingCharacters(in: textRange, with: string)
+
+        if updatedText.count > 5 {
+            return false
+        }
+
+        return true
+    }
+}
