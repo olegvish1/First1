f3691e198a5d87df83b59a8450ee4e4fbebf3d2a
Oleg <olegvish81@gmail.com>
2021-05-29 15:28:02 +0300

    add gradient and remove cats images

--- First1/SliderViewController.swift
+++ First1/SliderViewController.swift
@@ -34,8 +34,7 @@ class SliderViewController: UIViewController {
         coverView.alpha = CGFloat(slider.value)
         contentView.clipsToBounds = shadowSwitch.isOn
 
-        imageView.image = #imageLiteral(resourceName: "cat")
-        view.backgroundColor = #colorLiteral(red: 0, green: 0.7891016006, blue: 0.8377580643, alpha: 1)
+        createGradientLayer()
     }
 
     @IBAction func sliderAction(_ sender: UISlider) {
@@ -58,7 +57,17 @@ class SliderViewController: UIViewController {
     }
 
     @IBAction func switchAction(_ sender: UISwitch) {
+
         contentView.clipsToBounds = sender.isOn
+
+        UIView.animate(
+            withDuration: 2.0,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: { [weak self] in
+
+                self?.imageView.alpha = sender.isOn ? 1 : 0
+        })
     }
 }
 
@@ -67,14 +76,33 @@ extension SliderViewController {
     func setupContentView() {
 
         contentView.layer.cornerRadius = 15
+        contentView.clipsToBounds = false
 
         addShadow()
     }
     func addShadow() {
         contentView.layer.shadowColor = UIColor.black.cgColor
-        contentView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        contentView.layer.shadowOffset = .zero
         contentView.layer.shadowRadius = 15
-        contentView.layer.shadowOpacity = 0.5
+        contentView.layer.shadowOpacity = 1
+    }
+
+    func createGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+
+        gradientLayer.frame = coverView.bounds
+        gradientLayer.startPoint = CGPoint(x: 0.3, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.7, y: 1)
+
+        gradientLayer.type = .radial
+
+        gradientLayer.colors = [
+            UIColor.red.cgColor,
+            UIColor.green.cgColor,
+            UIColor.blue.cgColor
+        ]
+
+        coverView.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 
