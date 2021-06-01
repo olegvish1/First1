ea13d1c80c64b82d162d1d09f413d6c98c16d839
Oleg <olegvish81@gmail.com>
2021-06-01 21:06:18 +0300

    change comments

--- First1/SceneDelegate.swift
+++ First1/SceneDelegate.swift
@@ -12,9 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    //  kadfjbvsdklvn
+    //  kadfjbvsdklvn1
 
-//    asdvasjb
+//    asdvasjb2
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
