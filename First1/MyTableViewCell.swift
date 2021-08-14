51b280d9190770696767201c29edd8f4ef8dc6f9
Oleg <olegvish81@gmail.com>
2021-08-14 12:01:45 +0300

    table view cell polishing

--- First1/MyTableViewCell.swift
+++ First1/MyTableViewCell.swift
@@ -15,7 +15,8 @@ class MyTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        myImageView.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
